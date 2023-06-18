ee00d6b19f2c57cf242e6bd8b544a7e1a42aef7b
queendoescode <132417286+queendoescode@users.noreply.github.com>
2023-06-18 00:12:28 -0400

    check for null,validate length,and added questions so the user can select character types

--- script.js
+++ script.js
@@ -15,6 +15,21 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-  var length = prompt("length of password, between 8 and 128 characters")
-
+  var length = prompt("length of password, between 8 and 128 characters");
+  if (length === null) {
+    // User clicked cancel in the prompt
+    return;
+  } else {
+    if (length >= 8 && length <= 128) {
+      // Length is OK
+      // proceed to the other prompts
+      var includeLowercase = confirm("Do you want lowercase characters?");
+      var includeUppercase = confirm("Do you want uppercase characters?");
+      var includeNumeric = confirm("Do you want digits?");
+      var includeSpecial = confirm("Do you want special characters?");
+    } else {
+      // User entered nonsense
+      alert("Please enter a number between 8 and 128.");
+    }
+  }
 }
