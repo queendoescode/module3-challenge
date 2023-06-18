a0b17c6af15bdb2d78183dc2388d2066e45d404f
queendoescode <132417286+queendoescode@users.noreply.github.com>
2023-06-17 23:42:58 -0400

    added prompt for length

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +13,8 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var length = prompt("length of password, between 8 and 128 characters")
+
+}
