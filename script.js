0e5faa76e769bfd07947a2cf1d1364265c9aa92c
queendoescode <132417286+queendoescode@users.noreply.github.com>
2023-06-18 01:12:50 -0400

    added validation to the character types

--- script.js
+++ script.js
@@ -14,11 +14,15 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// The generatePassword() function prompts the user for length and character types
+// and then returns the generated password.
+// If the user's inputs were unacceptable or cancel, then return false.
+
 function generatePassword() {
   var length = prompt("length of password, between 8 and 128 characters");
   if (length === null) {
     // User clicked cancel in the prompt
-    return;
+    return false;
   } else {
     if (length >= 8 && length <= 128) {
       // Length is OK
@@ -27,9 +31,20 @@ function generatePassword() {
       var includeUppercase = confirm("Do you want uppercase characters?");
       var includeNumeric = confirm("Do you want digits?");
       var includeSpecial = confirm("Do you want special characters?");
+
+      if (includeLowercase || includeUppercase || includeNumeric || includeSpecial) {
+        // User selected at least one character type;
+        // Now we are ready to generate the password.
+        return newPassword;
+      } else {
+        // User did not select any character types
+        alert("You need to select at least one character type for your password.");
+        return false;
+      }
     } else {
       // User entered nonsense
       alert("Please enter a number between 8 and 128.");
+      return false;
     }
   }
 }
