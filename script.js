79c5004787d638b258defedd8d8ddfb62c4f2e6f
queendoescode <132417286+queendoescode@users.noreply.github.com>
2023-06-19 22:50:32 -0400

    broke long functions into two simpler functions for better code structure

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// These variables will store the user's selected options
+var length;
+var includeLowercase;
+var includeUppercase;
+var includeNumeric;
+var includeSpecial;
+
 
 // Write password to the #password input
 function writePassword() {
@@ -13,6 +20,39 @@ function writePassword() {
 
 }
 
+// Assuming valid options selected by the user,
+// build the random password and return it as 
+// the value of this function.
+
+function makePassword() {
+  var requestedCharacters = "";
+
+  if (includeLowercase) {
+    requestedCharacters = requestedCharacters + "abcdefghijklmnopqrstuvwxyz";
+  }
+
+  if (includeUppercase) {
+    requestedCharacters = requestedCharacters + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+
+  if (includeNumeric) {
+    requestedCharacters = requestedCharacters + "0123456789";
+  }
+
+  if (includeSpecial) {
+    requestedCharacters = requestedCharacters + " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  }
+
+  var newPassword = "";
+
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * requestedCharacters.length);
+    newPassword = newPassword + requestedCharacters[randomIndex];
+  }
+
+  return newPassword;
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
@@ -21,7 +61,7 @@ generateBtn.addEventListener("click", writePassword);
 // If the user's inputs were unacceptable or cancel, then return false.
 
 function generatePassword() {
-  var length = prompt("length of password, between 8 and 128 characters");
+  length = prompt("length of password, between 8 and 128 characters");
   if (length === null) {
     // User clicked cancel in the prompt
     return false;
@@ -29,41 +69,15 @@ function generatePassword() {
     if (length >= 8 && length <= 128) {
       // Length is OK
       // proceed to the other prompts
-      var includeLowercase = confirm("Do you want lowercase characters?");
-      var includeUppercase = confirm("Do you want uppercase characters?");
-      var includeNumeric = confirm("Do you want digits?");
-      var includeSpecial = confirm("Do you want special characters?");
+      includeLowercase = confirm("Do you want lowercase characters?");
+      includeUppercase = confirm("Do you want uppercase characters?");
+      includeNumeric = confirm("Do you want digits?");
+      includeSpecial = confirm("Do you want special characters?");
 
       if (includeLowercase || includeUppercase || includeNumeric || includeSpecial) {
         // User selected at least one character type;
         // Now we are ready to generate the password.
-
-        var requestedCharacters = "";
-
-        if (includeLowercase) {
-          requestedCharacters = requestedCharacters + "abcdefghijklmnopqrstuvwxyz";
-        }
-
-        if (includeUppercase) {
-          requestedCharacters = requestedCharacters + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        }
-
-        if (includeNumeric) {
-          requestedCharacters = requestedCharacters + "0123456789";
-        }
-
-        if (includeSpecial) {
-          requestedCharacters = requestedCharacters + " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-        }
-
-        var newPassword = "";
-
-        for (var i = 0; i < length; i++) {
-          var randomIndex = Math.floor(Math.random() * requestedCharacters.length);
-          newPassword = newPassword + requestedCharacters[randomIndex];
-        }
-
-        return newPassword;
+        return makePassword();
       } else {
         // User did not select any character types
         alert("You need to select at least one character type for your password.");
